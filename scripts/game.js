c8f815f64ffd189a8d92070ad3b7a5c6309a2e1d
KieranCG <>
2023-09-25 10:18:50 +0000

    Java scripts and tests of basic game functionality added

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,45 @@
 let game = {
-    score: 0
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    turnNumber: 0,
+    choices: ["button1", "button2", "button3", "button4"],
 };
 
-module.exports = {game};
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+    addTurn();
+};
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+};
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+};
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        };
+    }, 800);
+};
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -3,7 +3,7 @@
  */
 
 // Import the 'game' object from an external module (e.g., '../game')
-const { game } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
 // Set up the DOM or any necessary environment before running tests
 beforeAll(() => {
@@ -21,5 +21,70 @@ describe("Game option contains correct keys", () => {
     test("score key exists", () => {
         // Ensure that the 'score' key exists in the 'game' object
         expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain the correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    });
+});
+
+describe("New game works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear playerMoves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should be one move in the computer array", () => {
+        expect(game.currentGame.length).toBe(1);
+    });
+    test("should display 0 for the element with the id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    });
+});
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
+    });
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
     });
 });
